43cfa193a25979f48a6fa0eae91e4d3ae1a4f3a3
thompson.wu <thompson.wu@cardinfolink.com>
2019-10-29 17:27:40 +0800

    修改template

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wordConvert
-word文件文本提取
+word文件文本提取生成html
 #### 安装
 ```
 npm install -g wwl-word
@@ -9,3 +9,4 @@ npm install -g wwl-word
 word -f xx.docx  // 指定文件转换
 word             // 当前目录下的所有docx文件
 ```
+> 在当前目录下生成`wordConvert`文件夹
--- index.js
+++ index.js
@@ -8,74 +8,19 @@ const shelljs = require('shelljs')
 
 const { exec } = shelljs
 
-const HEADTMPLATE = `
-<!DOCTYPE html>
+const HEADTMPLATE = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>用户服务协议</title>
-  <style>
-    * {
-      margin: 0;
-      padding: 0;
-    }
-    body {
-      font-size: 14px;
-    }
-    p {
-      font-size: 0.14rem;
-      color: rgba(0,0,0,.8);
-    }
-    .container {
-      padding: 0.2rem 0.2rem 0.8rem 0.2rem;
-    }
-    .title {
-      text-align: center;
-      line-height: 1.8;
-      font-size: 0.25rem;
-      color: #000;
-      font-weight: 400;
-    }
-    .title-2 {
-      font-weight: bold;
-    }
-    /* .p::first-line {
-      text-indent: 0.5rem;
-    } */
-    a {
-      color: #208dff;
-    }
-  </style>
+  <title>title</title>
 </head>
 <body>
   <div class="container">
 `
 const FOOTERTMPLATE = `
   </div>
-<script>
-    function initRem() {
-      var clientWidth = document.documentElement.clientWidth
-      var rem = clientWidth / 750
-      document.documentElement.style.fontSize = rem * 100 + 'px'
-    }
-    function addIndent() {
-      var paragraphs = document.getElementsByClassName('p')
-      for(var i = 0; i < paragraphs.length; i++) {
-        var textNode = paragraphs[i].firstChild
-        var span = document.createElement('span')
-        span.innerText = 'span'
-        span.style.color = '#fff'
-        span.style.display = 'inline-block'
-        span.style.width = '0.3rem'
-        paragraphs[i].insertBefore(span, textNode)
-      }
-    }
-    initRem()
-    addIndent()
-    window.addEventListener('resize', initRem, false)
-</script>
 </body>
 </html>
 `
